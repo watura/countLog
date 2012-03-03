600837a65b9522a6f6a6b8c2de02f3f247a85368
NISHIMOTO Wataru <watura@g91.org>
2012-03-03 20:04:11 +0900

    Initial Commit

---
+++ countLog/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  countLog
+//
+//  Created by Wataru Nishimoto on 12/03/03.
+//  Copyright (c) 2012年 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ countLog/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  countLog
+//
+//  Created by Wataru Nishimoto on 12/03/03.
+//  Copyright (c) 2012年 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
